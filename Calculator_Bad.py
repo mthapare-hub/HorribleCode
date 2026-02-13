f26119e01e91bb5f52f465e8cbb7b17249fc4962
Marvin Thapa Renteral <mtrenteral@gmail.com>
2026-02-13 17:48:21 -0500

    Added one bad coding practice

--- Calculator_Bad.py
+++ Calculator_Bad.py
@@ -1,12 +1,13 @@
 class Calculator:
 
     def add_and_sub(self, x, y, addOrSub):
+        """Bad code because it does not follow the idea of single responsibility."""
         if(addOrSub == "add"):
             return x + y
         elif(addOrSub == "sub"):
             return x - y
 
-    
+
 
     def multiply(self, x, y):
         return x * y

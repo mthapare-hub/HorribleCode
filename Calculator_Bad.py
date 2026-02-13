fb3aaeffa74f30050f521d53ca0ceb62738cf7f2
Marvin Thapa Renteral <mtrenteral@gmail.com>
2026-02-13 17:44:19 -0500

    adding initial code

---
+++ Calculator_Bad.py
@@ -0,0 +1,21 @@
+class Calculator:
+
+    def add_and_sub(self, x, y, addOrSub):
+        if(addOrSub == "add"):
+            return x + y
+        elif(addOrSub == "sub"):
+            return x - y
+
+    
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        return x / y
+
+    def square(self, x):
+        return x ** 2
+
+
+Calculator = Calculator()

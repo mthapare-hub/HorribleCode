a717ff832cf3cf7b00a2306d55d3b0b49ff69efd
samueljr3 <samueljr3@hotmail.com>
2026-02-13 20:18:57 -0500

    Added DRY method example

--- Calculator_Good.py
+++ Calculator_Good.py
@@ -9,4 +9,19 @@ class Calculator:
     
 
 
+    def multiply(self, x, y):
+        return x * y
+    
+    def divide(self, x, y):
+        return x / y
+
+    def square(self, x):
+        return x ** 2
+    
+    def add_five(self, x):
+        """This code is DRY because it was not repeating the same code"""
+        return x + 5
+    
+
+    
 Calculator = Calculator()

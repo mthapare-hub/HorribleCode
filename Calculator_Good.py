e4f54b04a86336b728b83274393a14fa0349c8d1
samueljr3 <samueljr3@hotmail.com>
2026-02-13 19:53:24 -0500

    added methods that show single responsibility

---
+++ Calculator_Good.py
@@ -0,0 +1,12 @@
+class Calculator:
+
+    """Good code because it follows the idea of single responsibility."""
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+    
+
+
+Calculator = Calculator()
